8cfabe1662ad9f1beda6dbfd4d3f0bb82175daca
Guilherme Biegelmeyer <guilhermebiegelmeyer@gmail.com>
2021-08-14 15:10:45 -0300

    configure session to login

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import productRouter from '@modules/product/routes/product.routes';
 import userRouter from '@modules/user/routes/user.routes';
+import sessionsRouter from '@modules/user/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
 
 routes.use('/users', userRouter);
+
+routes.use('/login', sessionsRouter);
 
 routes.get('/', (request, response) => {
     return response.json({ message: new Date() });
